ec2dd9e3fbb4e63a453a771621e32bd2ad5b3fd9
Pacome Terrier <pacome.terrier1@etu.univ-lorraine.fr>
2026-03-27 11:35:07 +0100

    refactor: suppression des derniers 'if' dans EvenementPeriodique (100% streams)

--- MyCalendar/src/main/java/mycalendar/domain/EvenementPeriodique.java
+++ MyCalendar/src/main/java/mycalendar/domain/EvenementPeriodique.java
@@ -19,38 +19,29 @@ public class EvenementPeriodique extends Evenement {
 
     @Override
     public boolean estDansPeriode(PeriodeRecherche periode) {
-        DateHeureDebut dateOccurence = dateDebut();
-        
-        // Optimisation basique : on avance par bonds jusqu'à atteindre ou dépasser la période
-        while (dateOccurence.estAvant(periode.debut())) {
-            dateOccurence = dateOccurence.plusJours(frequence.jours());
-        }
+        // Calculer le nombre maximum d'occurrences à tester pour couvrir la période
+        // (Approximation simple pour éviter le while initial)
+        long joursEntre = java.time.temporal.ChronoUnit.DAYS.between(dateDebut().valeur(), periode.fin().valeur());
+        int maxOccurrences = (int) (joursEntre / frequence.jours()) + 1;
 
-        // Vérifier si cette occurrence ou l'une des suivantes est dans la période
-        while (!dateOccurence.estApres(periode.fin())) {
-            DateHeureDebut finOccurence = dateOccurence.plusMinutes(duree().minutes());
-            if (!periode.debut().estApres(finOccurence) && !periode.fin().estAvant(dateOccurence)) {
-                return true;
-            }
-            dateOccurence = dateOccurence.plusJours(frequence.jours());
-        }
-        return false;
+        return java.util.stream.IntStream.range(0, maxOccurrences)
+                .mapToObj(i -> dateDebut().plusJours(i * frequence.jours()))
+                .anyMatch(occurence -> {
+                    DateHeureDebut finOccurence = occurence.plusMinutes(duree().minutes());
+                    return !periode.debut().estApres(finOccurence) && !periode.fin().estAvant(occurence);
+                });
     }
 
     @Override
     protected boolean estEnConflitInterne(DateHeureDebut autreDebut, DureeEvenement autreDuree) {
         int maxOccurrencesTestables = 365 / frequence.jours() + 1;
-        DateHeureDebut dateOccurence = dateDebut();
 
-        for (int i = 0; i < maxOccurrencesTestables; i++) {
-            DateHeureDebut fin1 = dateOccurence.plusMinutes(duree().minutes());
-            DateHeureDebut fin2 = autreDebut.plusMinutes(autreDuree.minutes());
-
-            if (dateOccurence.estAvant(fin2) && fin1.estApres(autreDebut)) {
-                return true;
-            }
-            dateOccurence = dateOccurence.plusJours(frequence.jours());
-        }
-        return false;
+        return java.util.stream.IntStream.range(0, maxOccurrencesTestables)
+                .mapToObj(i -> dateDebut().plusJours(i * frequence.jours()))
+                .anyMatch(occurence -> {
+                    DateHeureDebut fin1 = occurence.plusMinutes(duree().minutes());
+                    DateHeureDebut fin2 = autreDebut.plusMinutes(autreDuree.minutes());
+                    return occurence.estAvant(fin2) && fin1.estApres(autreDebut);
+                });
     }
 }
